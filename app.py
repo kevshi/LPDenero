a0f53e4b36c99da777a86fa87a68de7ff578caa2
Sanjay Sreekumar <sanjays1@berkeley.edu>
2015-03-01 20:34:28 -0800

    added changes to html and py

--- app.py
+++ app.py
@@ -5,35 +5,39 @@ from searchalgorithm import search, searchAllFiles
 
 app = Flask(__name__)
 app.database = "videotext.db"
+query = ''
 
 #homepage
 @app.route('/', methods = ['GET', 'POST'])
 def home():
-
-	#database, initializes video
-	g.db = connect_db()
-	cur = g.db.execute('select * from videos')
-	videos = [[row[0], row[1]] for row in cur.fetchall()]
-	query = 'course'
-	for video in videos:
-		k = search(video[0], query)
-		video[1] = k
-		video[0] = video[0].replace('./videofiles/', '')
-		video[0] = video[0].replace('.txt', '')
-
-	# error = None
-	# if request.method == 'POST':
-	# 	if query == '':
-	# 		error = 'Did not input search, please re-enter.'
-	# 	else:
-	# 		return redirect(url_for('result'))
-	return render_template('index.html')
+	error = None
+	if request.method == 'POST':
+		global query 
+		query = request.form['search']
+		if request.form['search'] == '':
+			error = 'Did not input search, please re-enter.'
+		else:
+			return redirect(url_for('result'))
+	return render_template('index.html', error=error)
 
 
 #results page
 @app.route('/result')
 def result():
-	return render_template('result.html')
+	#database, initializes video
+	g.db = connect_db()
+	cur = g.db.execute('select * from videos')
+	videos = [[row[0], row[1]] for row in cur.fetchall()]
+	count = 0
+	for video in videos:
+		k = search(video[0], query)
+		video[1] = k
+		video[0] = video[0].replace('./videofiles/', '')
+		video[0] = video[0].replace('.txt', '')
+	for video in videos:
+		count += video[1]
+
+	return render_template('result.html', title = video[0], description = video[1])
 
 #creates database object
 def connect_db():
